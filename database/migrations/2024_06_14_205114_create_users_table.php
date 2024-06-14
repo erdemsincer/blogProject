462a9f67e8ddebd76f774153cbe9518f49385385
erdemsincer <erdemsincer123@icloud.com>
2024-06-15 02:07:26 +0300

    Users Tablosuna role_id eklendi

--- database/migrations/2024_06_14_205114_create_users_table.php
+++ database/migrations/2024_06_14_205114_create_users_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-
-
+            $table->unsignedBigInteger('role_id');
             $table->string('name');
             $table->string('surname');
             $table->string('username')->unique();
@@ -24,6 +23,7 @@ return new class extends Migration
             $table->string('profile_photo');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('role_id')->references('id')->on('roles');
 
 
         });
